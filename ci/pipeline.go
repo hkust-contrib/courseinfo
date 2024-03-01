086e6341028a6c2aa0f4d3f76f1ca0a1b655b7da
Kristopher Lam <root@sandb0x.io>
2024-03-01 23:56:20 +0800

    switch to dagger build

---
+++ ci/pipeline.go
@@ -0,0 +1,42 @@
+package main
+import (
+	"context"
+	"log/slog"
+	"os"
+	"dagger.io/dagger"
+)
+
+func main() {
+	if err := build(context.Background()); err != nil {
+		slog.Error("An error occurred while running pipeline", err)
+	}
+}
+
+func build(ctx context.Context) error {
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+	src := client.Host().Directory(".")
+	builder := client.Container().From("golang:1.22")
+	builder = builder.WithDirectory("/src", src).WithWorkdir("/src")
+	builder = builder.WithExec([]string{"go", "mod", "download"})
+	builder = builder.WithEnvVariable("CGO_ENABLED", "0").WithEnvVariable("GOOS", "linux").WithEnvVariable("GOARCH", "amd64")
+	builder = builder.WithExec([]string{"go", "build", "-o", "bin/acch"})
+	runtime := client.Container().From("gcr.io/distroless/base-debian12")
+	runtime = runtime.WithWorkdir("/app")
+	runtime = runtime.WithFile("/app/bin/acch", builder.File("/src/bin/acch"))
+	runtime = runtime.WithExposedPort(8080)
+	runtime = runtime.WithEntrypoint([]string{"/app/bin/acch"})
+
+	secret := client.SetSecret("password", os.Getenv("CI_REGISTRY"))
+	runtime = runtime.WithRegistryAuth(os.Getenv("CI_REGISTRY"), os.Getenv("CI_REGISTRY_USER"), secret)
+	image, err := runtime.Publish(ctx, os.Getenv("CI_REGISTRY_IMAGE"))
+	slog.Info("Successfully published image at ", image)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
--- go.mod
+++ go.mod
@@ -1,21 +1,30 @@
 module github.com/hkust-cse/crapi
 
-go 1.20
+go 1.21
+
+toolchain go1.22.0
 
 require (
+	dagger.io/dagger v0.10.0
 	github.com/gocolly/colly/v2 v2.1.0
 	github.com/labstack/echo/v4 v4.11.4
 )
 
 require (
+	github.com/99designs/gqlgen v0.17.31 // indirect
+	github.com/Khan/genqlient v0.6.0 // indirect
+	github.com/adrg/xdg v0.4.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.6 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 )
