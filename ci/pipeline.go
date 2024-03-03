f24cee82f82cde1cc18972a692d8c3dba8395dcd
Kristopher <root@sandb0x.io>
2024-03-03 01:57:59 +0000

    Update pipeline.go

--- ci/pipeline.go
+++ ci/pipeline.go
@@ -31,7 +31,7 @@ func build(ctx context.Context) error {
 	runtime = runtime.WithEntrypoint([]string{"/app/bin/acch"})
 
 	secret := client.SetSecret("password", os.Getenv("CI_REGISTRY_PASSWORD"))
-	for i, tag := range []string{"latest", os.Getenv("CI_COMMIT_SHORT_SHA")} {
+	for _, tag := range []string{"latest", os.Getenv("CI_COMMIT_SHORT_SHA")} {
 		urn := fmt.Sprintf("%s:%s", os.Getenv("CI_REGISTRY_IMAGE"), tag)
 		image, err := runtime.WithRegistryAuth(os.Getenv("CI_REGISTRY"), os.Getenv("CI_REGISTRY_USER"), secret).Publish(ctx, urn)
 		slog.Info("Successfully published", "image", image)
